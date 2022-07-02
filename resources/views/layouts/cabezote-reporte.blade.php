a0068b181b34a87b0d692d9be7187bb5c063ba57
carlos <carlosruiz288910@gmail.com>
2022-07-02 14:03:12 -0500

    cambios factura

--- resources/views/layouts/cabezote-reporte.blade.php
+++ resources/views/layouts/cabezote-reporte.blade.php
@@ -1,13 +1,13 @@
 <div class="cabezote__grid">
     <div class="cabezote__titulo">FACTURA</div>
     <div class="cabezote__logo">TPTECH</div>
-    <div class="cabezote__fecha">30/06/2022</div>
+    <div class="cabezote__fecha">{{ date ( 'd-m-Y' ); }}</div>
 </div>
 
 <div class="cabezote__cliente">
-    <p><b>Cliente:</b> Fulanito de tal</p>
-    <p><b>Dirección:</b> Carrera 123 # 45 - 67</p>
-    <p><b>Teléfono:</b> 3155555555</p>
+    <p><b>Cliente:</b>{{ Auth::User()->name }}</p>
+    <p><b>Dirección:</b> {{ Auth::User()->dire }} </p>
+    <p><b>Teléfono:</b> {{ Auth::User()->tel }}</p>
 </div>
 
 <h2 class="cabezote__detalle_fact">Detalle Factura</h2>
